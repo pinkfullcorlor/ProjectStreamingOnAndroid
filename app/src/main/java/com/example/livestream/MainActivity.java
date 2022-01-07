6c4b3a01c3a096174fc4f7728eabfc036d9e596a
QuocVuong0910 <quocvuongcum@gmail.com>
2022-01-07 23:27:20 +0700

    Update-popup
    Vuong-11:30-1/7

--- app/src/main/java/com/example/livestream/MainActivity.java
+++ app/src/main/java/com/example/livestream/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.livestream;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
     ArrayList<SectionDataModel> allSampleData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,34 @@ public class MainActivity extends AppCompatActivity {
         MyDataAdapter adapter = new MyDataAdapter(this, allSampleData);
         myRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         myRecyclerView.setAdapter(adapter);
+
+    }
+
+    public void showPopup(View v){
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.popup_menu);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item4:
+                Toast.makeText(this, "Item 4 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
     }
     //Tạo dữ liệu
     public void createDummyData() {
@@ -60,6 +93,8 @@ public class MainActivity extends AppCompatActivity {
             allSampleData.add(dm);
 
         }
+
+
         
 //        String[] items = {"item1", "item2", "item3"};
 //        VideoView videoView = findViewById(R.id.video_view);
